2f18be8920c16ca4464e217dceccc88f730a58ef
William Cory <willcory10@gmail.com>
2018-05-23 12:08:58 -0700

    complete python server

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import requests
-import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
-import pandas as pd
 from flask import Flask
+
+from surprise import Dataset, Reader, SVD
+from surprise.model_selection import cross_validate
+import numpy as np
+import pandas as pd
+
+from collections import defaultdict
 
 url = 'https://hrr30-enzyme-backend.herokuapp.com/analytics'
 
@@ -10,36 +14,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Machines Learn!"
+    return "hello"
 
 @app.route('/recomendations')
-def recomendations():
+def update_recomendations():
+
+    # get the data from the server
     data = requests.get(url).json()
-    return update_recomendations(data.pastData, data.currentData)
+    X = pd.DataFrame(data)
 
-def update_recomendations(pastData, currentData):
-
-    pastdf = pd.read_json(pastData)
-    currentData = pd.read_json(currentData)
+    # prepare data for training
+    reader = Reader(rating_scale=(0, 100))
+    data = Dataset.load_from_df(X[['uid', 'qid', 'rating']], reader)
+    data.split(n_folds=5)
     
-    X = np.array()
-    y = np.array()
+    # train the model
+    algo = SVD()
+    cross_validate(algo, data, measures=['RMSE', 'MAE'])
+    trainset = data.build_full_trainset()
+    algo.fit(trainset)
 
-    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+    # now predict ratings for all users and questions not in the training set
+    testset = trainset.build_anti_testset()
+    predictions = algo.test(testset)
 
-    clf = neighbors.KNeighborsClassifier()
-    clf.fit(X_train, y_train)
+    top_n = get_top_n(predictions, n=10)
 
-    # see how we did
-    accuracy = clf.score(X_test, y_test)
-    print(accuracy)
+    return top_n
 
-    # now we are making examples
-    example_measures = np.array([])
+def get_top_n(predictions, n=10):
+    top_n = defaultdict(list)
 
-    # now we can make predictions
-    prediction = clf.predict(example_measures)
-    print(prediction)
+    # map the predictions to the users
+    for uid, iid, r_ui, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # sort and find the top 10
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
